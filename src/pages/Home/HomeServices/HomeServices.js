8155e17a7444fb9d5d9c2b730445bda9e74b9317
Felix Odete <felix.odette@gmail.com>
2023-01-21 19:45:45 +0300

    Services text

--- src/pages/Home/HomeServices/HomeServices.js
+++ src/pages/Home/HomeServices/HomeServices.js
@@ -10,20 +10,20 @@ const HomeServices = () => {
                   <div className="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
                      <div className="section__title section__title-3 text-center mb-90 wow fadeInUp" data-wow-delay=".2s">
                         <span>Our Services</span>
-                        <h2>Provide awesome customer service with our tools.</h2>
+                        <h2>We psrovide awesome service with exceptional staff.</h2>
                      </div>
                   </div>
                </div>
                <div className="row">
 
-                  <SingleService icon="1" title="Expense reporting" />
-                  <SingleService icon="2" title="Tax Advisory" />
-                  <SingleService icon="3" title="Consulting" />
-                  <SingleService icon="4" title="Audit & Assurance" />
-                  <SingleService icon="5" title="Finance Analytics" />
-                  <SingleService icon="6" title="Cord Processing" />
-                  <SingleService icon="7" title="Development" />
-                  <SingleService icon="8" title="Financial audit" />
+                  <SingleService icon="1" title="Service reporting" />
+                  <SingleService icon="2" title="Team Leaders" />
+                  <SingleService icon="3" title="Porters" />
+                  <SingleService icon="4" title="Kitchen Team" />
+                  <SingleService icon="5" title="Chefs All Levels" />
+                  <SingleService icon="6" title="Concierge" />
+                  <SingleService icon="7" title="Coordinators" />
+                  <SingleService icon="8" title="Reception" />
 
                </div>
             </div>
