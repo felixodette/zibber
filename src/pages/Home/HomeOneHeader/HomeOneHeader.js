4fc86413972fbac85d021c318855409f2f3f0f5d
Felix Odete <felix.odette@gmail.com>
2023-01-21 18:44:52 +0300

    Add gold logo for onscroll

--- src/pages/Home/HomeOneHeader/HomeOneHeader.js
+++ src/pages/Home/HomeOneHeader/HomeOneHeader.js
@@ -58,12 +58,12 @@ const HomeOneHeader = () => {
                         <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6">
                            <div className="logo">
                               <NavLink to="/">
-                                 <img src="assets/img/logo/Griffin - Logo.png" alt="logo"/>
+                                 <img style={{width: "105"}} src="assets/img/logo/Griffin - Logo.png" alt="logo"/>
                               </NavLink>
                            </div>
                            <div className="logo-gradient">
                               <NavLink to="/">
-                                 <img src="assets/img/logo/logo-gradient.png" alt="logo"/>
+                                 <img src="assets/img/logo/Griffin - Gold Logo.png" alt="logo"/>
                               </NavLink>
                            </div>
                         </div>
