053b7ff8c04799d4aca80f8c74c43abc59c679df
Felix Odete <felix.odette@gmail.com>
2023-01-22 11:20:25 +0300

    Update location

--- src/pages/Contact/ContactArea/ContactArea.js
+++ src/pages/Contact/ContactArea/ContactArea.js
@@ -8,7 +8,7 @@ const ContactArea = () => {
                <div className="row g-0">
                   <div className="col-xl-12">
                      <div className="contact__map">
-                        <iframe title='contact' src="https://maps.google.com/maps?hl=en&amp;q=Dhaka+()&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed"></iframe>
+                        <iframe title='contact' src="https://maps.google.com/maps?hl=en&amp;q=London+()&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed"></iframe>
                         <div className="contact__wrapper d-md-flex justify-content-between wow fadeInUp" data-wow-delay=".3s">
                            <div className="contact__info mr-100">
                               <h3>Get in touch</h3>
