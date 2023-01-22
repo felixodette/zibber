65bf5bd03cb0c5c1003240f0b10ae2a253e23ea8
Felix Odete <felix.odette@gmail.com>
2023-01-22 09:12:16 +0300

    Services page text

--- src/components/HomeThreeSecondSingleService/HomeThreeSecondSingleService.js
+++ src/components/HomeThreeSecondSingleService/HomeThreeSecondSingleService.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const HomeThreeSecondSingleService = ({title}) => {
+const HomeThreeSecondSingleService = ({title, paragraph}) => {
    return (
       <>
          <div className="services__nav-content pt-90 pb-90">
@@ -14,14 +14,14 @@ const HomeThreeSecondSingleService = ({title}) => {
                <div className="col-xl-7 col-lg-6">
                   <div className="services__content-3 pl-70 pr-70">
                      <h3>{title}</h3>
-                     <p>The bee's knees cockup loo cracking goal hotpot bits and bobs a load of old tosh bubble and squeak arse over tit twit, excuse my French bevvy Eaton argy-bargy amongst codswallop.</p>
+                     <p>{paragraph}</p>
                      <div className="services__icon-wrapper d-md-flex mb-35">
                         <div className="services__icon-item d-flex mr-60 mb-30">
                            <div className="icon mr-20">
                               <img src="assets/img/icon/services/services-9.png" alt="" />
                            </div>
                            <div className="text">
-                              <h3>Acquisitions Finance Consulting</h3>
+                              <h3>Hospitality Training Consulting</h3>
                            </div>
                         </div>
                         <div className="services__icon-item d-flex mb-30">
@@ -29,7 +29,7 @@ const HomeThreeSecondSingleService = ({title}) => {
                               <img src="assets/img/icon/services/services-10.png" alt="" />
                            </div>
                            <div className="text">
-                              <h3>Private Placement Consulting</h3>
+                              <h3>Hospitality Placement Consulting</h3>
                            </div>
                         </div>
                      </div>
--- src/pages/HomeThree/HomeThreeSecondServices/HomeThreeSecondServices.js
+++ src/pages/HomeThree/HomeThreeSecondServices/HomeThreeSecondServices.js
@@ -11,8 +11,8 @@ const HomeThreeSecondServices = () => {
                <div className="row">
                   <div className="col-xl-6">
                      <div className="section-title section__title-3 mb-70">
-                        <h2>We Help <br/> Clients Make Profit</h2>
-                        <p>We help you weather today's uncertainty through our best team.</p>
+                        <h2>We Help <br/> Clients Recruit</h2>
+                        <p>We help you through today's recruitment uncertainty using our best team.</p>
                      </div>
                   </div>
                </div>
@@ -22,22 +22,22 @@ const HomeThreeSecondServices = () => {
                         <ul className="nav nav-pills " id="services-tab" role="tablist">
                            <li className="nav-item mb-45">
                               <a className="nav-link active" id="share-tab" data-bs-toggle="pill" href="#share" role="tab" aria-controls="share" aria-selected="true">
-                                 <i> <SiKnowledgebase/> </i>Share Knowledge
+                                 <i> <SiKnowledgebase/> </i>Meet and Define
                               </a>
                            </li>
                            <li className="nav-item mb-30">
                               <a className="nav-link" id="customer-tab" data-bs-toggle="pill" href="#customer" role="tab" aria-controls="customer" aria-selected="true">
-                                 <i ><BsTools/></i> Customer Service
+                                 <i ><BsTools/></i> Search and Screen
                               </a>
                            </li>
                            <li className="nav-item mb-30">
                               <a className="nav-link" id="social-tab" data-bs-toggle="pill" href="#social" role="tab" aria-controls="social" aria-selected="true">
-                                 <i ><BsHeart/></i> Social Media
+                                 <i ><BsHeart/></i> Finalise and Appoint
                               </a>
                            </li>
                            <li className="nav-item mb-30">
                               <a className="nav-link" id="financial-tab" data-bs-toggle="pill" href="#financial" role="tab" aria-controls="financial" aria-selected="true">
-                                 <i ><BsBag/></i> Financial
+                                 <i ><BsBag/></i> Train and Support
                               </a>
                            </li>
                         </ul>
@@ -47,16 +47,16 @@ const HomeThreeSecondServices = () => {
                      <div className="services__tab grey-bg-18">
                         <div className="tab-content" id="services-content">
                            <div className="tab-pane fade show active" id="share" role="tabpanel" aria-labelledby="share-tab">
-                              <HomeThreeSecondSingleService title="Share Knowledge" />
+                              <HomeThreeSecondSingleService title="Meet and Define" paragraph="We get a full understanding of your company, the job vacancy and the candidate profile you are searching for. We want to be your partner in understanding your industry and the challenges you face." />
                            </div>
                            <div className="tab-pane fade" id="customer" role="tabpanel" aria-labelledby="customer-tab">
-                              <HomeThreeSecondSingleService title="Customer Service" />
+                              <HomeThreeSecondSingleService title="Search and Screen" paragraph="We reach out to our candidate network after identifying the best profile matches. Recruiters interview prospective candidates and hand-pick those with the right competencies for your position." />
                            </div>
                            <div className="tab-pane fade" id="social" role="tabpanel" aria-labelledby="social-tab">
-                              <HomeThreeSecondSingleService title="Social Media" />
+                              <HomeThreeSecondSingleService title="Finalise and Appoint" paragraph="We present you with a shortlist of candidates with the best fit to the job and organizational culture and will support you to coordinate interviews, shortlist candidates and make the final job offer." />
                            </div>
                            <div className="tab-pane fade" id="financial" role="tabpanel" aria-labelledby="financial-tab">
-                              <HomeThreeSecondSingleService title="Financial" />
+                              <HomeThreeSecondSingleService title="Train and Support" paragraph="Would you like additional support through training? We can also apply a 360 degree approach to helping you fill your job vacancy though our Talent Search, Selection and Job Induction service." />
                            </div>
                         </div>
                      </div>
